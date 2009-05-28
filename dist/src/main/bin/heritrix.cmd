7ca3fbff72b3019f915f3c7b5e7e4de72ab8276a
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2009-05-28 00:05:44 +0000

    * heritrix.cmd     remove JMX-related paths; streamline comments; link to Heritrix3-on-Windows wiki page

--- dist/src/main/bin/heritrix.cmd
+++ dist/src/main/bin/heritrix.cmd
@@ -1,31 +1,15 @@
-:: This script launches the heritrix crawler on windows.  While Heritrix
-:: is unsupported on windows, see 2.1.1.3 in the User Manual
-:: [http://crawler.archive.org/articles/user_manual.html], this script was
-:: provided by Eric Jensen as a convenience to the windows-afflicted.
+:: This script launches the Heritrix crawler on windows.  While Heritrix
+:: is not officially supported on Windows, as a Java application it 
+:: generally works (and a number of active contributors primarily use
+:: Windows for Heritrix development). 
 ::
-:: It is a direct translation of the heritrix linux wrapper script -- and
-:: because windows is not supported on Heritrix, it will likely lag the unix
-:: start script.
+:: This script was originally provided by Eric Jensen as a convenience 
+:: to the Windows-afflicted. Its proper functioning/functionality may
+:: lag the officially-supported Linux start script. Contributions of
+:: improvements are always welcome.
 ::
 :: See also:
-:: https://sourceforge.net/tracker/index.php?func=detail&aid=1514538&group_id=73833&atid=539102
-::
-:: Versions:
-::
-:: 2006-07-17  Original Version by Eric Jensen
-::
-:: 2006-08-04  Disclaimer added by Michael Stack
-::
-:: 2006-08-28  A few fixes by Max Schöfmann:
-::             - command extensions and veriable expansion are automatically
-::               enabled
-::             - JMX configuration fixed (not the fancy "sed" stuff however)
-::             - Try to set permissions of JMX password file if Heritrix
-::               fails to start and JMX is enabled
-::             - a few more small improvements (java detection, fake background
-::               execution...)
-::             - comments changed from rem to :: and file renamed to .cmd 
-::               (to make clear it won't work on Win 9x...)
+:: http://webarchive.jira.com/wiki/display/Heritrix/Heritrix3+on+Windows
 ::
 ::
 ::  Optional environment variables
@@ -45,19 +29,6 @@
 ::  FOREGROUND       Set to any value -- e.g. 'true' -- if you want to run 
 ::                   heritrix in foreground (Used by build system when it runs
 ::                   selftest to see if completed successfully or not).
-:: 
-::  JMX_OPTS         Default is to startup the JVM JMX administration 
-::                   on port 8849 if the JVM is SUN JVM 1.5.  This allows JMX
-::                   administration of Heritrix.  If the JVM is other than the
-::                   SUN JDK 1.5, the arguments are ignored. If you do not want
-::                   to start the JVM JXM administration server on the SUN JDK
-::                   1.5, set this variable to empty string.
-:: 
-::  JMX_PORT         Port you'd like the JVM JMX administration server to run
-::                   on. Default is 8849.
-:: 
-::  JMX_OFF          Set to a non-empty string to disable JMX (and JMX setup of
-::                   password file, etc.)
 :: 
 @echo off
 
@@ -140,33 +111,6 @@ set CLASSPATH=!CP!
 if not defined JAVA_OPTS set JAVA_OPTS= -Xmx256m
 
 :: Setting environment vars in nested IFs is error prone, thus using GOTOs
-if not defined JMX_OFF goto configure_jmx
-goto jmx_configured
-
-:configure_jmx
-if not defined JMX_PORT set JMX_PORT=8849
-if not defined JMX_OPTS set JMX_OPTS=-Dcom.sun.management.jmxremote.port=%JMX_PORT% -Dcom.sun.management.jmxremote.ssl=false "-Dcom.sun.management.jmxremote.password.file=%HERITRIX_HOME%\jmxremote.password"
-
-:: DONT Copy into place a jmxremote password file that uses the heritrix password
-:: interpolated (First need to find the current password if one supplied on
-:: command-line, else use whats in heritrix.properties as default).
-:: Need to make it so its only readable by user else jconsole won't use it.
-:: JMX_PASSWORD=`echo "%@" |sed -n -e 's/.*--admin=[^:]*:\([^ ]*\).*/\1/p' -e 's/.*-a *[^:]*:\([^ ]*\).*/\1/p'`
-:: if [ -z "%JMX_PASSWORD" ]
-:: then
-::  JMX_PASSWORD=`sed -n -e 's/heritrix.cmdline.admin[ ]*=[^:]*:\(.*\)/\1/p' \
-::  %{HERITRIX_HOME}\conf\heritrix.properties`
-:: fi
-:: JMX_PWORD_FILE="%{HERITRIX_HOME}\jmxremote.password"
-:: if [ -f "%{JMX_PWORD_FILE}" ]
-::  then
-:: rm -f "%{JMX_PWORD_FILE}"
-:: fi
-:: sed -e "s/@PASSWORD@/%{JMX_PASSWORD}/" \
-::  "%{HERITRIX_HOME}\conf\jmxremote.password.template" > "%{JMX_PWORD_FILE}"
-:: chmod 600 "%{JMX_PWORD_FILE}"
-
-:jmx_configured
 
 :: Main heritrix class.
 if not defined CLASS_MAIN set CLASS_MAIN=org.archive.crawler.Heritrix
@@ -180,7 +124,6 @@ set startMessage=%HERITRIX_HOME%\heritrix_dmesg.log
 
 :: Remove any file that may have been left over from previous starts.
 if exist "%startMessage%" del "%startmessage%"
-if exist "%HERITRIX_HOME%\jmx_permissions_broken" del "%HERITRIX_HOME%\jmx_permissions_broken"
 
 :: Run heritrix as daemon.  Redirect stdout and stderr to a file.
 :: Print start message with date, java version, java opts, ulimit, and uname.
@@ -197,10 +140,10 @@ echo JAVA_OPTS=%JAVA_OPTS% >>"%stdouterrlog%"
 :: if defined FOREGROUND
 :start_heritrix
 if not defined FOREGROUND goto run_in_background
-%JAVACMD% "-Dheritrix.home=%HERITRIX_HOME%" -Djava.protocol.handler.pkgs=org.archive.net "-Dheritrix.out=%HERITRIX_OUT%" %JAVA_OPTS% %JMX_OPTS% %CLASS_MAIN% %HERITRIX_CMDLINE%
+%JAVACMD% "-Dheritrix.home=%HERITRIX_HOME%" -Djava.protocol.handler.pkgs=org.archive.net "-Dheritrix.out=%HERITRIX_OUT%" %JAVA_OPTS% %CLASS_MAIN% %HERITRIX_CMDLINE%
 :: errorlevel 130 if aborted with Ctrl+c (at least my sun jvm 1.5_07...)
 if errorlevel 130 goto :end
-if errorlevel 1 goto fix_jmx_permissions
+if errorlevel 1 goto fix_problems
 goto :end
 
 :run_in_background
@@ -210,9 +153,9 @@ if not "%1"=="BGR" (
 ) else (
     title Heritrix
     :: adding  ">>%stdouterrlog% 2>&1" causes an access denied error as heritrix writes also to this file	
-    %JAVACMD% "-Dheritrix.home=%HERITRIX_HOME%" -Djava.protocol.handler.pkgs=org.archive.net "-Dheritrix.out=%HERITRIX_OUT%" %JAVA_OPTS% %JMX_OPTS% %CLASS_MAIN% %HERITRIX_CMDLINE%	
+    %JAVACMD% "-Dheritrix.home=%HERITRIX_HOME%" -Djava.protocol.handler.pkgs=org.archive.net "-Dheritrix.out=%HERITRIX_OUT%" %JAVA_OPTS% %CLASS_MAIN% %HERITRIX_CMDLINE%	
     if errorlevel 130 goto :end
-    if errorlevel 1 echo.!ERRORLEVEL! >"%HERITRIX_HOME%\jmx_permissions_broken"
+    if errorlevel 1 echo.!ERRORLEVEL! >"%HERITRIX_HOME%\heritrix_launch_problems"
 	pause
 	)
 goto :end
@@ -226,9 +169,9 @@ echo.
 echo %DATE% %TIME% Starting heritrix
 :print_logfile
 %SLEEP%>nul
-if exist "%HERITRIX_HOME%\jmx_permissions_broken" (
-    del "%HERITRIX_HOME%\jmx_permissions_broken"
-    goto fix_jmx_permissions
+if exist "%HERITRIX_HOME%\heritrix_launch_problems" (
+    del "%HERITRIX_HOME%\heritrix_launch_problems"
+    goto fix_problems
 )
 if exist "%startMessage%" (
     %SLEEP%>nul
@@ -251,29 +194,13 @@ del "%startMessage%" >nul 2>&1
 :: del doesn't set the ERRORLEVEL var if unsuccessful, so we can't try again
 goto :end
 
-:fix_jmx_permissions
+:fix_problems
 if not "%CLASS_MAIN%"=="org.archive.crawler.Heritrix" goto :start_may_failed
-if defined PERMISSIONS_FIXED goto fix_jmx_permission_failed
 echo.
 echo Heritrix failed to start properly. Possible causes:
 echo.
-echo - another programm uses the port for the web inferface (8080 default)
+echo - another program uses the port for the web inferface (8443 default)
 echo   (e.g. another Heritrix instance)
-if defined JMX_OFF goto :end
-echo - permissions problem with the JMX password file. 
-echo.
-set /P FIXIT=Do you want to try to fix the permissions (Y/N)?
-if /I "%FIXIT:~0,1%"=="n" goto :end
-cacls "%HERITRIX_HOME%\jmxremote.password" /P %USERNAME%:R
-if errorlevel 1 goto fix_jmx_permission_failed
-set PERMISSIONS_FIXED=true
-set /P RESTART=Restart Heritrix (Y/N)?
-if /I "%RESTART:~0,1%"=="y" goto start_heritrix
-goto :end
-
-:fix_jmx_permission_failed
-set PERMISSIONS_FIXED=
-echo Either fixing the permissions failed or there was another problem
 goto :end
 
 :start_may_failed
