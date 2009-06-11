56b31490c8862c5bfaed899b685e11208cba3bf4
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2009-06-11 23:06:07 +0000

    [HER-1574] meta refresh urls not html unescaped * ExtractorHTML.java     apply same 1-line fix as H1/H2

--- modules/src/main/java/org/archive/modules/extractor/ExtractorHTML.java
+++ modules/src/main/java/org/archive/modules/extractor/ExtractorHTML.java
@@ -769,6 +769,7 @@ public class ExtractorHTML extends ContentExtractor implements InitializingBean 
                 (attr.start(14) > -1) ? 14 : (attr.start(15) > -1) ? 15 : 16;
             CharSequence value =
                 cs.subSequence(attr.start(valueGroup), attr.end(valueGroup));
+            value = TextUtils.unescapeHtml(value);
             if (attr.group(1).equalsIgnoreCase("name")) {
                 name = value.toString();
             } else if (attr.group(1).equalsIgnoreCase("http-equiv")) {
