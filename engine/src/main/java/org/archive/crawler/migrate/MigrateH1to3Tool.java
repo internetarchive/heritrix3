47cbdded9a96c50cc416475e9c2cb74950e24545
gojomo <gojomo@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2009-10-07 04:28:10 +0000

    [HER-1641] migration tool to convert Heritrix 1.14.x order.xml to H3 crawler-beans.cxml * MigrateH1to3Tool.java     comment, clean unused code * H1toH3.map     text map of {H3-override-beanpath, H1-settings-XPath, H1-default-value} * migrate-template-crawler-beans.cxml     version of bundled profile slightly changed for better mapping to H1     (target replace range, seeds.txt, arcWriter)

--- engine/src/main/java/org/archive/crawler/migrate/MigrateH1to3Tool.java
+++ engine/src/main/java/org/archive/crawler/migrate/MigrateH1to3Tool.java
@@ -1,27 +1,59 @@
+/*
+ *  This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ *  Licensed to the Internet Archive (IA) by one or more individual 
+ *  contributors. 
+ *
+ *  The IA licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.archive.crawler.migrate;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.LineIterator;
 import org.apache.commons.lang.StringUtils;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
+/**
+ * Utility class which takes a H1 order.xml and creates a similar
+ * H3 job directory, with as many simple settings converted over
+ * (as top-of-crawler-beans overrides) as possible at this time.
+ * 
+ * (Future versions will handle more complicated H1 settings
+ * customizations, such as per-host overrides or choices of 
+ * alternate implementing classes for Scope, Processors, etc.)
+ * 
+ * @contributor siznax
+ * @contributor gojomo
+ */
 public class MigrateH1to3Tool {
     static DocumentBuilder DOCUMENT_BUILDER;
     static {
@@ -32,195 +64,87 @@ public class MigrateH1to3Tool {
         }
     }
 
-    // TODO: fixxx
-	protected File templateH3job = new File("/Users/steve/Documents/workspace-3.4.2/h3/dist/src/main/conf/jobs/profile-defaults"); 
-	protected File defaultBeansXmlFile = new File(templateH3job,"profile-crawler-beans.cxml");
-
-	protected File sourceOrderXmlFile;
-	protected File destinationH3JobDir;
-	protected File targetBeansXmlFile;
-
-	protected Document sourceOrderXmlDom;
-	protected Document targetBeansXmlDom;
-
+    public static void main(String[] args) throws Exception {
+        new MigrateH1to3Tool().instanceMain(args);
+    }
+    
 	public void instanceMain(String[] args) throws Exception {
 		
-		sourceOrderXmlFile = new File(args[0]);
-		destinationH3JobDir = new File(args[1]);
-        if(args.length>2) {
-            templateH3job = new File(args[2]); 
+        File sourceOrderXmlFile = new File(args[0]);
+		File destinationH3JobDir = new File(args[1]);
+        
+        destinationH3JobDir.mkdirs();
+        
+        InputStream inStream = getClass().getResourceAsStream(
+                "migrate-template-crawler-beans.cxml");
+        String template = IOUtils.toString(inStream);
+        inStream.close(); 
+        
+        Map<String,String> migrateH1toH3Map = getMigrateMap(); 
+        
+        Document sourceOrderXmlDom = DOCUMENT_BUILDER.parse(sourceOrderXmlFile);
+        Map<String,String> h1simpleSettings = flattenH1Order(sourceOrderXmlDom);
+        
+        StringBuilder sb = new StringBuilder(); 
+        for(String key : h1simpleSettings.keySet()) {
+            String beanPath = migrateH1toH3Map.get(key);
+            if(beanPath==null) {
+                System.err.println("no rule found for: "+key);
+            } else if (beanPath.startsWith("$")) {
+                System.err.println(beanPath.substring(1)+": "+key);
+            } else if (beanPath.startsWith("*")) {
+                // TODO: needs special handling
+                if(beanPath.equals("*metadata.userAgentTemplate")) {
+                    splitH1userAgent(h1simpleSettings.get(key),sb); 
+                } else {
+                    System.err.println(beanPath+": "+key);
+                }
+            } else {
+                sb
+                 .append(beanPath)
+                 .append("=")
+                 .append(h1simpleSettings.get(key))
+                 .append("\n");
+            }
         }
         
-		targetBeansXmlFile = new File(destinationH3JobDir,"crawler-beans.cxml");
-
-		FileUtils.copyFile(defaultBeansXmlFile, targetBeansXmlFile);
-				
-        sourceOrderXmlDom = DOCUMENT_BUILDER.parse(sourceOrderXmlFile);
+        // patch all overrides derived from H1 into H3 template
+        String beansCxml = template.replace("###MIGRATE_OVERRIDES###", sb.toString());
         
-		domFindOrderXmlControllerStrings(sourceOrderXmlDom);
-		domFindOrderXmlScopeElements(sourceOrderXmlDom);
-		xpathFindOrderXmlScopeNode(sourceOrderXmlDom);
-
-        targetBeansXmlDom = DOCUMENT_BUILDER.parse(targetBeansXmlFile);
-		System.out.println("targetBeansXmlDom = " + targetBeansXmlDom.getLocalName());
-		unRollNode(targetBeansXmlDom.getElementsByTagName("property"));
-
-		migrateMetadata();
-
+        File targetBeansXmlFile = new File(destinationH3JobDir,"crawler-beans.cxml");
+        FileUtils.writeStringToFile(targetBeansXmlFile, beansCxml);
+        
+        FileUtils.copyFile(
+                    new File(sourceOrderXmlFile.getParentFile(), "seeds.txt"), 
+                    new File(destinationH3JobDir, "seeds.txt"));
 	}
 
-    protected void unRollNode(NodeList node) {
-		System.out.println("unRollNode " + node.toString()); // + node.getLength());
-		for (int i=0; i<node.getLength(); i++) {
-			System.out.println(node.item(i).getNodeName()
-					+ " " + node.item(i).getNodeValue()
-					+ " " + node.item(i).getTextContent());
-			for ( int j=0; j<node.item(i).getChildNodes().getLength(); j++) {
-				Node beanChild = node.item(i).getChildNodes().item(j);
-				System.out.println(" > " + beanChild.getNodeName()
-						+ " " + beanChild.getNodeValue()
-						+ " " + beanChild.getTextContent());
-			}
-		}
-	}
-
-	protected void setBeanProperty(String id, String property, String value) 
-	throws Exception {
-		System.out.println("setBeanProperty("+id+","+property+","+value+")");
-
-		// String xpathStr = "//bean[@id=\""+id+"\"]//property[@name=\""+property+"\"]";
-		String xpathStr = "//bean[@id=\"" + id + "\"]";
-		System.out.println("  xpathStr = " + xpathStr);
-		NodeList node = getBeanNode(xpathStr);
-		unRollNode(node);
-		
-		}
-	
-	protected NodeList getBeanNode(String xpath) throws Exception {
-		return getDocNode(targetBeansXmlDom,xpath);
-	}
-
-	protected NodeList getDocNode(Document doc, String str) throws Exception {
-		System.out.println("getDocNode(" + doc.getLocalName() + "," + str + " )");
-		XPathFactory factory = XPathFactory.newInstance();
-		XPath xpath = factory.newXPath();
-        try {
-    		XPathExpression expr = xpath.compile(str);
-    		NodeList list = (NodeList) expr.evaluate(doc,XPathConstants.NODE);
-    		System.out.println("xpath \"" + str + "\" found " + list.getLength() + " members");
-    		return list;
-        } catch (XPathExpressionException e) {
-        	System.out.println("XPathExpressionException!");
-            return null;
-	}
-	}
-	
-	protected String getDocString(Document doc, String str) {
-		XPathFactory factory = XPathFactory.newInstance();
-		XPath xpath = factory.newXPath();
-        try {
-            XPathExpression expr = xpath.compile(str);
-            return expr.evaluate(doc);
-        } catch (XPathExpressionException e) {
-            return null;
-        }
-	}
-	
-	// JOB_NAME => order_meta_name = bean_metadata_jobName
-	// DESCRIPTION => order_meta_description = bean_metadata_description
-	protected void migrateMetadata() throws Exception {
-
-		String order_meta_name = getDocString(sourceOrderXmlDom,"//meta/name");
-		String order_meta_description = getDocString(sourceOrderXmlDom,"//meta/description");
-		System.out.println("order_meta/name = " + order_meta_name);
-		System.out.println("order_meta/description = " + order_meta_description);
-		
-//		NodeList bean_simpleOverrides = getBeanNode("//bean[@=\"simpleOverrides\"]");
-//		unRollNode(bean_simpleOverrides);
-
-		setBeanProperty("simpleOverrides","jobName",order_meta_name);
-		// setBeanProperty("metadata","description",order_meta_description);
-	}
-	
-	// CONTACT_URL => map_http-headers_from = bean_metadata_operatorContactUrl
-	// HVERSION => map_http-headers_user-agent=" heritrix/(.*) " => 
-	protected void migrateHttpHeaders() throws Exception {
-	}
-
-//	protected void migrateControllerStrings()
-//	protected void migrateScope()
-
-	protected void xpathFindOrderXmlScopeNode(Document orderXmlDom) throws Exception {
-		// use Xpath to extract scope node
-        XPathFactory factory = XPathFactory.newInstance();
-        XPath xpath = factory.newXPath();
-		String xp = "//controller/newObject[@name=\"scope\"]";
-		System.out.print("==== XPATH {" + xp + "} ====\n");
-        XPathExpression expr = xpath.compile(xp);
-        NodeList scope = (NodeList) expr.evaluate(orderXmlDom,XPathConstants.NODE);
-		for (int j=0; j< scope.getLength(); j++) {
-			if (scope.item(j).getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {
-				System.out.println("  " + scope.item(j).getNodeName());
-			}
-		}
-	}
-	
-	protected void domFindOrderXmlScopeElements(Document orderXmlDom) {
-		Element crawlOrder = orderXmlDom.getDocumentElement();
-		NodeList newObjects = crawlOrder.getElementsByTagName("newObject");
-		for (int i=0; i<newObjects.getLength(); i++) {
-			if (newObjects.item(i).hasAttributes()) {
-				Node name = newObjects.item(i).getAttributes().getNamedItem("name");
-				
-				// scope
-				if (name.getNodeValue().equalsIgnoreCase("scope")) {
-					String npath = newObjects.item(i).getNodeName()
-					+ "[" + i + "][@" + name.getNodeName() 
-					+ "=\"" + name.getNodeValue() + "\"]";
-					System.out.println("==== DOM search {" + npath + "} ====");
-					NodeList scope = newObjects.item(i).getChildNodes(); 
-					for (int j=0; j<scope.getLength(); j++) {
-						if (scope.item(j).getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {
-							System.out.println("  " + scope.item(j).getNodeName());
-						}
-					}
-				}
-				
-			}
-		}
-	}
-	
-	protected void domFindOrderXmlControllerStrings(Document orderXmlDom) {
-		Element crawlOrder = orderXmlDom.getDocumentElement();
-		NodeList strings = crawlOrder.getElementsByTagName("string");
-		for (int i=0; i<strings.getLength(); i++) {
-			if (strings.item(i).getParentNode().getNodeName() == "controller") {
-				if (strings.item(i).hasAttributes()) {
-					Node s = strings.item(i);
-					System.out.println(s.getParentNode().getNodeName()
-							+ "/"
-							+ s.getNodeName()
-							+ "[" + i + "]" 										
-							+ "@" 
-							+ s.getAttributes().item(0).getNodeName()
-							+ "["
-							+ s.getAttributes().item(0).getNodeValue() 
-							+ "]"
-							+ " = "
-							+ s.getTextContent());
-				}
-			}
-		}
-	}
-	
-	public static void main(String[] args) throws Exception {
-		new MigrateH1to3Tool().instanceMain(args);
-	}
-	
-    public static Map<String,String> flattenH1Order(String path) throws Exception {
-        return flattenH1Order(DOCUMENT_BUILDER.parse(new File(path)));
+    protected void splitH1userAgent(String userAgent, StringBuilder sb) {
+        String originalUrl = userAgent.replaceAll(
+                "^.*?\\+(http://[^)]*).*$",
+                "$1");
+        String newTemplate = userAgent.replace(originalUrl,"@OPERATOR_CONTACT_URL@");
+        sb
+         .append("metadata.operatorContactUrl=")
+         .append(originalUrl)
+         .append("\n")
+         .append("metadata.userAgentTemplate=")
+         .append(newTemplate)
+         .append("\n");
     }
 
+    protected Map<String, String> getMigrateMap() throws IOException {
+        Map<String,String> map = new HashMap<String,String>();
+        InputStream inStream = getClass().getResourceAsStream("H1toH3.map");
+        LineIterator iter = IOUtils.lineIterator(inStream, "UTF-8");
+        while(iter.hasNext()) {
+            String[] fields = iter.nextLine().split("\\|");
+            map.put(fields[1], fields[0]);
+        }
+        inStream.close();
+        return map;
+    }
+	
     /**
      * Given a Document, return a Map of all non-blank simple text 
      * nodes, keyed by the pseudo-XPath to their parent element. 
@@ -230,7 +154,7 @@ public class MigrateH1to3Tool {
      * @throws XPathExpressionException
      */
     public static Map<String,String> flattenH1Order(Document h1order) throws XPathExpressionException {
-        Map<String,String> flattened = new HashMap<String,String>();
+        Map<String,String> flattened = new LinkedHashMap<String,String>();
         XPathExpression xpath = XPathFactory.newInstance().newXPath().compile("//text()");
         NodeList nodes = (NodeList) xpath.evaluate(h1order,XPathConstants.NODESET);
         for(int i = 0; i< nodes.getLength(); i++) {
@@ -239,15 +163,15 @@ public class MigrateH1to3Tool {
                 String pseudoXPath = getPseudoXpath(node.getParentNode());
                 pseudoXPath = pseudoXPath.replaceFirst("/crawl-order", "/");
                 
-                System.out.println(
-                        pseudoXPath
-                        +" "+node.getTextContent());
+//                System.out.println(
+//                        pseudoXPath
+//                        +" "+node.getTextContent());
                 
-                flattened.put(getPseudoXpath(node.getParentNode()), node.getTextContent());
+                flattened.put(pseudoXPath, node.getTextContent());
             }
         }
-        System.err.println(flattened.size());
-        System.err.println(flattened);
+//        System.err.println(flattened.size());
+//        System.err.println(flattened);
         
         return flattened;
     }
