4802e5093e83c7cf6b1b8e519692cbeb3dcbd0a7
szznax <szznax@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2010-03-11 01:45:49 +0000

    part of fix for HER-1748 Arc2Warc should generally write WARC "response" records for ARC HTTP responses * Arc2Warc.java     changed namedField "ARC-Offset" to "ARC-Compressed-Offset" for clarity

--- commons/src/main/java/org/archive/io/Arc2Warc.java
+++ commons/src/main/java/org/archive/io/Arc2Warc.java
@@ -155,7 +155,7 @@ throws IOException {
            r.getHeaderString());
        ar.addLabelValue("ARC-File", 
            r.getMetaData().getArc());
-       ar.addLabelValue("ARC-Offset", 
+       ar.addLabelValue("ARC-Compressed-Offset", 
            String.valueOf(r.getHeader().getOffset()));
        ar.addLabelValue("ARC-Length", 
            String.valueOf(r.getHeader().getLength()));
