cc8ea6494a37ecf4a91492f6c7e8a1ef2d00ffa6
szznax <szznax@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2010-03-11 15:59:10 +0000

    changed namedField "ARC-Compressed-Offset" to "ARC-Gzip-Offset" for even more clarity

--- commons/src/main/java/org/archive/io/Arc2Warc.java
+++ commons/src/main/java/org/archive/io/Arc2Warc.java
@@ -155,7 +155,7 @@ throws IOException {
            r.getHeaderString());
        ar.addLabelValue("ARC-File", 
            r.getMetaData().getArc());
-       ar.addLabelValue("ARC-Compressed-Offset", 
+       ar.addLabelValue("ARC-Gzip-Offset", 
            String.valueOf(r.getHeader().getOffset()));
        ar.addLabelValue("ARC-Length", 
            String.valueOf(r.getHeader().getLength()));
