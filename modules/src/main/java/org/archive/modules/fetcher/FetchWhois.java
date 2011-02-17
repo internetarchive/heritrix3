368ff65286a0acbaa424f2dc645f8ed04808ffdb
nlevitt <nlevitt@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2011-02-17 22:52:37 +0000

    * FetchWhois.java     add license preamble

--- modules/src/main/java/org/archive/modules/fetcher/FetchWhois.java
+++ modules/src/main/java/org/archive/modules/fetcher/FetchWhois.java
@@ -1,3 +1,22 @@
+/*
+ *  This file is part of the Heritrix web crawler (crawler.archive.org).
+ *
+ *  Licensed to the Internet Archive (IA) by one or more individual 
+ *  contributors. 
+ *
+ *  The IA licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.archive.modules.fetcher;
 
 import java.io.BufferedReader;
@@ -148,7 +167,6 @@ public class FetchWhois extends Processor implements CoreAttributeConstants,
         } else {
             addWhoisLinks(curi);
             return ProcessResult.PROCEED;
-
         }
     }
 
@@ -239,7 +257,7 @@ public class FetchWhois extends Processor implements CoreAttributeConstants,
                     // gets rid of "domain " for whois.verisign-grs.com queries
                     String key = whoisQuery.replaceFirst("(\\S+\\s+)+", "").toLowerCase();
                     referralServers.put(key, matcher.group(1).toLowerCase());
-                    if (logger.isLoggable(Level.INFO)) {
+                    if (logger.isLoggable(Level.FINE)) {
                         logger.fine("added referral server " + matcher.group(1) + " to server list for " + key);
                     }
                 }
