a09c8e69b834c98c8331431b7251f53605cd0ba0
nlevitt <nlevitt@daa5b2f2-a927-0410-8b2d-f5f262fa301a>
2011-10-12 17:52:45 +0000

    * Recorder.java     remove TODOs that have already been done

--- commons/src/main/java/org/archive/util/Recorder.java
+++ commons/src/main/java/org/archive/util/Recorder.java
@@ -464,9 +464,6 @@ public class Recorder {
     /**
      * Get a replay cued up for the 'content' (after all leading headers)
      * 
-     * TODO: handle chunking
-     * TODO: handle decompression, either here or in a parallel method
-     * 
      * @return A replay input stream.
      * @throws IOException
      */
