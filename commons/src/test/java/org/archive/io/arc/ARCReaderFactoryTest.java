69eb124b5db734ba06622cba6b02e34186e26d52
Noah Levitt <nlevitt@archive.org>
2013-03-27 14:48:21 -0700

    add test for handling of bad http headers (to make sure we don't break this when we get rid of archive-overlay)

--- commons/src/test/java/org/archive/io/arc/ARCReaderFactoryTest.java
+++ commons/src/test/java/org/archive/io/arc/ARCReaderFactoryTest.java
@@ -19,6 +19,7 @@
 
 package org.archive.io.arc;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -27,6 +28,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
 
+import org.apache.commons.httpclient.HttpParser;
 import org.archive.io.ArchiveReader;
 import org.archive.io.ArchiveRecord;
 import org.archive.util.TmpDirTestCase;
@@ -123,6 +125,72 @@ public class ARCReaderFactoryTest extends TmpDirTestCase {
         assertEquals(200, r.getStatusCode());
         assertEquals("http://www.archive.org/test/", r.getHeader().getHeaderValue("subject-uri"));
         
+        assertFalse(i.hasNext());
+        reader.close();
+    }
+
+    private static final String ARC_RECORD_BAD_HEADERS = 
+            "filedesc://NARA-PEOT-2004-20041014205819-00000-crawling009.archive.org.arc 0.0.0.0 20041014205819 text/plain 1507\n"
+                    + "1 1 InternetArchive\n"
+                    + "URL IP-address Archive-date Content-type Archive-length\n"
+                    + "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+                    + "<arcmetadata xmlns=\"http://archive.org/arc/1.0/\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:dcterms=\"http://purl.org/dc/terms/\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:arc=\"http://archive.org/arc/1.0/\" xsi:schemaLocation=\"http://archive.org/arc/1.0/ http://www.archive.org/arc/1.0/arc.xsd\">\n"
+                    + "<arc:software>Heritrix 1.0.5-200410121100 http://crawler.archive.org</arc:software>\n"
+                    + "<arc:hostname>crawling009.archive.org</arc:hostname>\n"
+                    + "<arc:ip>207.241.227.210</arc:ip>\n"
+                    + "<dcterms:isPartOf>NARA-GOV-CRAWL-A</dcterms:isPartOf>\n"
+                    + "<dc:description>United States National Archives and Records Administration Presidential End of Term Web Harvest</dc:description>\n"
+                    + "<arc:operator>Igor Ranitovic</arc:operator>\n"
+                    + "<dc:publisher>Internet Archive</dc:publisher>\n"
+                    + "<dcterms:audience>United States National Archives and Records Administration</dcterms:audience>\n"
+                    + "<dc:date xsi:type=\"dcterms:W3CDTF\">2004-10-14T20:58:03+00:00</dc:date>\n"
+                    + "<arc:http-header-user-agent>Mozilla/5.0 (compatible; heritrix/1.0.5-200410121100 +http://www.archives.gov/crawl.html)</arc:http-header-user-agent>\n"
+                    + "<arc:http-header-from>archive-crawler-agent@lists.sourceforge.net</arc:http-header-from>\n"
+                    + "<arc:robots>ignore</arc:robots>\n"
+                    + "<dc:format>ARC file version 1.1</dc:format>\n"
+                    + "<dcterms:conformsTo xsi:type=\"dcterms:URI\">http://www.archive.org/web/researcher/ArcFileFormat.php</dcterms:conformsTo>\n"
+                    + "</arcmetadata>\n"
+                    + "\n"
+                    + "\n"
+                    + "http://schrock.house.gov/PollTemplate_PollAnswers_1 143.231.169.175 20041014211845 text/html 387\n"
+                    + "HTTP/1.1 302 Moved Temporarily\r\n"
+                    + "Server: Microsoft-IIS/5.0\r\n"
+                    + "Date: Thu, 14 Oct 2004 21:21:24 GMT\r\n"
+                    + "Location: /PollTemplate_PollAnswers_1/\r\n"
+                    + "HTTP/1.1 404 Object Not Found\r\n"
+                    + "Server: Microsoft-IIS/5.0\r\n"
+                    + "Date: Thu, 14 Oct 2004 21:21:24 GMT\r\n"
+                    + "Content-Type: text/html\r\n"
+                    + "Content-Length: 108\r\n"
+                    + "\r\n"
+                    + "<html><head><title>Object Not Found</title></head><body><h1>HTTP/1.1 404 Object Not Found</h1></body></html>\n";
+
+    /**
+     * This test exercises handling of bad http headers currently handled in the
+     * archive-overlay version of
+     * {@link HttpParser#parseHeaders(java.io.InputStream, String)}, which
+     * wayback cdx indexer depends on
+     */
+    public void testBadArcHeaders() throws IOException {
+        ByteArrayInputStream in = new ByteArrayInputStream(ARC_RECORD_BAD_HEADERS.getBytes("UTF-8"));
+        ArchiveReader reader = ARCReaderFactory.get(null, in, false);
+        assertNotNull(reader);
+
+        Iterator<ArchiveRecord> i = reader.iterator();
+
+        // ARC header
+        assertTrue(i.hasNext());
+        ARCRecord r = (ARCRecord)i.next();
+        assertEquals("filedesc://NARA-PEOT-2004-20041014205819-00000-crawling009.archive.org.arc", r.getHeader().getHeaderValue("subject-uri"));
+
+        // record with 2 http status lines
+        assertTrue(i.hasNext());
+        r = (ARCRecord)i.next();
+        assertEquals("http://schrock.house.gov/PollTemplate_PollAnswers_1", r.getHeader().getHeaderValue("subject-uri"));
+        assertEquals(302, r.getStatusCode());
+        assertEquals("HttpClient-Bad-Header-Line-Failed-Parse", r.getHttpHeaders()[3].getName());
+        assertEquals("HTTP/1.1 404 Object Not Found", r.getHttpHeaders()[3].getValue());
+
         assertFalse(i.hasNext());
         reader.close();
     }
