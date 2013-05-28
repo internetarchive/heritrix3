internetarchive/heritrix3

commit 73f58dc56aebe333567b097ab3b7747360e0abd8

scripting console: fix bug: exception from script execution is not displayed. remove extra space around exception and rawOutput. involves a bit more than bug fix. introduced ScriptExecution class for encapsulating script, execution, and result.

engine/src/main/java/org/archive/crawler/restlet/ScriptResource.java
@@ -19,7 +19,6 @@
 
 package org.archive.crawler.restlet;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -38,14 +37,10 @@ import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 import org.archive.crawler.framework.BeanLookupBindings;
-import org.archive.crawler.restlet.models.CrawlJobModel;
-import org.archive.crawler.restlet.models.EngineModel;
 import org.archive.crawler.restlet.models.ScriptModel;
 import org.archive.crawler.restlet.models.ViewModel;
-import org.archive.util.TextUtils;
 import org.restlet.Context;
 import org.restlet.data.CharacterSet;
 import org.restlet.data.Form;
@@ -89,11 +84,13 @@ public class ScriptResource extends JobRelatedResource {
             }
         });
     }
-    protected String script = "";
-    protected Exception ex = null;
-    protected int linesExecuted = 0; 
-    protected String rawOutput = ""; 
-    protected String htmlOutput = "";
+    //protected String script = "";
+    //protected Exception ex = null;
+    //protected int linesExecuted = 0; 
+    //protected String rawOutput = ""; 
+    //protected String htmlOutput = "";
+    ScriptExecution scriptExec = null;
+    
     protected String chosenEngine = FACTORIES.isEmpty() ? "" : FACTORIES.getFirst().getNames().get(0);
     private Configuration _templateConfiguration;
     
@@ -115,48 +112,107 @@ public class ScriptResource extends JobRelatedResource {
         return _templateConfiguration;
     }
     
+    /**
+	 * JavaBean that packages script, its execution and the result.
+	 * 
+	 * TODO: the script could create an object that persists and retains a
+	 * reference to the Bindings. by encapsulating this way, I've made it
+	 * a bit more difficult? we could {@code eng} and {@code script} arguments
+	 * to execute() method.
+	 */
+    public static class ScriptExecution {
+    	private ScriptEngine eng;
+    	private ApplicationContext appCtx;
+    	private Bindings bindings;
+    	private String script;
+    	
+    	private StringWriter rawString;
+    	private StringWriter htmlString;
+    	private Throwable exception;
+    	private int linesExecuted;
+    	
+    	public ScriptExecution(ScriptEngine eng, ApplicationContext appCtx, String script) {
+    		this.eng = eng;
+    		this.appCtx = appCtx;
+    		this.bindings = new BeanLookupBindings(appCtx);
+    		this.script = script;
+    		if (StringUtils.isBlank(this.script)) {
+    			this.script = "";
+    		}
+    		this.rawString = new StringWriter();
+    		this.htmlString = new StringWriter();
+    	}
+    	public void bind(String name, Object obj) {
+    		bindings.put(name, obj);
+    	}
+    	public Object unbind(String name) {
+    		return bindings.remove(name);
+    	}
+    	public void execute() {
+    		PrintWriter rawOut = new PrintWriter(rawString);
+    		PrintWriter htmlOut = new PrintWriter(htmlString);
+    		bind("rawOut", rawOut);
+    		bind("htmlOut", htmlOut);
+    		bind("appCtx", appCtx);
+    		try {
+    			eng.eval(script, bindings);
+    			// TODO: should count with RE rather than creating String[]?
+    			linesExecuted = script.split("\r?\n").length;
+    		} catch (ScriptException ex) {
+    			Throwable cause = ex.getCause();
+    			exception = cause != null ? cause : ex;
+    		} catch (RuntimeException ex) {
+    			exception = ex;
+    		} finally {
+    			rawOut.flush();
+    			htmlOut.flush();
+    			// TODO: are these really necessary? 
+    			unbind("rawOut");
+    			unbind("htmlOut");
+    			unbind("appCtx");
+    		}
+    	}
+    	public boolean isFailure() {
+    		return exception != null;
+    	}
+    	public String getStackTrace() {
+    		if (exception == null) return "";
+    		StringWriter s = new StringWriter();
+    		exception.printStackTrace(new PrintWriter(s));
+    		return s.toString();
+    	}
+    	public Throwable getException() {
+    		return exception;
+    	}
+    	public int getLinesExecuted() {
+    		return linesExecuted;
+    	}
+    	public String getRawOutput() {
+    		return rawString.toString();
+    	}
+    	public String getHtmlOutput() {
+    		return htmlString.toString();
+    	}
+    }
+    
     @Override
     public void acceptRepresentation(Representation entity) throws ResourceException {
         Form form = getRequest().getEntityAsForm();
         chosenEngine = form.getFirstValue("engine");
-        script = form.getFirstValue("script");
+        String script = form.getFirstValue("script");
         if(StringUtils.isBlank(script)) {
             script="";
         }
 
         ScriptEngine eng = MANAGER.getEngineByName(chosenEngine);
         
-        StringWriter rawString = new StringWriter(); 
-        PrintWriter rawOut = new PrintWriter(rawString);
-        ApplicationContext appCtx = cj.getJobContext();
-        Bindings bindings = new BeanLookupBindings(appCtx);
-        bindings.put("rawOut", rawOut);
-        StringWriter htmlString = new StringWriter(); 
-        PrintWriter htmlOut = new PrintWriter(htmlString);
-        bindings.put("htmlOut", htmlOut);
-        bindings.put("job", cj);
-        bindings.put("appCtx", appCtx);
-        bindings.put("scriptResource", this);
-        try {
-            eng.eval(script, bindings);
-            linesExecuted = script.split("\r?\n").length;
-        } catch (ScriptException e) {
-            ex = e;
-        } catch (RuntimeException e) {
-            ex = e;
-        } finally {
-            rawOut.flush();
-            rawOutput = rawString.toString();
-            htmlOut.flush();
-            htmlOutput = htmlString.toString();
-
-            // the script could create an object that persists and retains a reference to the Bindings
-            bindings.put("rawOut", null);
-            bindings.put("htmlOut", null);
-            bindings.put("job", null);
-            bindings.put("appCtx", null);
-            bindings.put("scriptResource", null);
-        }
+        scriptExec = new ScriptExecution(eng, cj.getJobContext(), script);
+        scriptExec.bind("job", cj);
+        scriptExec.bind("scriptResource", this);
+        scriptExec.execute();
+        scriptExec.unbind("job");
+        scriptExec.unbind("scriptResource");
+        
         //TODO: log script, results somewhere; job log INFO? 
         
         getResponse().setEntity(represent());
@@ -211,10 +267,7 @@ public class ScriptResource extends JobRelatedResource {
         ScriptModel model = new ScriptModel(cj.getShortName(),
                 new Reference(baseRefRef, "..").getTargetRef().toString(),
                 getAvailableScriptEngines(),
-                linesExecuted,
-                ex,
-                rawOutput,
-                htmlOutput);
+                scriptExec);
 
         return model;
     }
@@ -235,7 +288,6 @@ public class ScriptResource extends JobRelatedResource {
         viewModel.put("baseResourceRef",getRequest().getRootRef().toString()+"/engine/static/");
         viewModel.put("model",makeDataModel());
         viewModel.put("selectedEngine", chosenEngine);
-        viewModel.put("script", script);
 
         try {
             Template template = tmpltCfg.getTemplate("Script.ftl");

engine/src/main/java/org/archive/crawler/restlet/models/ScriptModel.java
@@ -6,29 +6,23 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang.StringUtils;
+import org.archive.crawler.restlet.ScriptResource.ScriptExecution;
 
 @SuppressWarnings("serial")
 public class ScriptModel  extends LinkedHashMap<String, Object> {
 
-    public ScriptModel(String crawlJobShortName, String crawlJobUrl, Collection<Map<String,String>> scriptEngines, int linesExecuted, Exception exception, String rawOutput, String htmlOutput){
+	public ScriptModel(String crawlJobShortName, String crawlJobUrl,
+			Collection<Map<String, String>> scriptEngines,
+			ScriptExecution scriptExec) {// int linesExecuted, Exception
+											// exception, String rawOutput,
+											// String htmlOutput){
         super();
         this.put("crawlJobUrl",crawlJobUrl);
         this.put("crawlJobShortName", crawlJobShortName);
         this.put("availableScriptEngines", scriptEngines);
         
-        if (linesExecuted > 0) {
-            this.put("linesExecuted", linesExecuted);
-        }
-        if (exception != null) {
-            this.put("exception", exception);
-        }
-        if (StringUtils.isNotBlank(rawOutput)) {
-            this.put("rawOutput", rawOutput);
-        }
-        if (StringUtils.isNotBlank(htmlOutput)) {
-            this.put("htmlOutput", htmlOutput);
-        }
+        if (scriptExec != null)
+        	this.put("scriptExec", scriptExec);
         
         List<Map<String,String>> vars = new LinkedList<Map<String,String>>();
         Map<String,String> var;

commit c53b821c85ba5ba115d8e6848c7ad881f64b0666

FIX: 73f58dc56a broke XML variant of scripting console response, and will   produce incompatible structure (extra nesting with scriptExec element).   moved ScriptExecution code to ScriptModel, which is now a bean, not Map.   XmlMarshaller is extended to support simple JavaBeans to XML marshalling,   borrowing annotation classes from javax.xml.

engine/src/main/java/org/archive/crawler/restlet/ScriptResource.java
@@ -20,8 +20,6 @@
 package org.archive.crawler.restlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Collection;
 import java.util.Collections;
@@ -31,14 +29,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import javax.script.Bindings;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 
 import org.apache.commons.lang.StringUtils;
-import org.archive.crawler.framework.BeanLookupBindings;
 import org.archive.crawler.restlet.models.ScriptModel;
 import org.archive.crawler.restlet.models.ViewModel;
 import org.restlet.Context;
@@ -52,7 +47,6 @@ import org.restlet.resource.Representation;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.Variant;
 import org.restlet.resource.WriterRepresentation;
-import org.springframework.context.ApplicationContext;
 
 import freemarker.template.Configuration;
 import freemarker.template.ObjectWrapper;
@@ -84,7 +78,6 @@ public class ScriptResource extends JobRelatedResource {
             }
         });
     }
-    ScriptExecution scriptExec = null;
     
     protected String chosenEngine = FACTORIES.isEmpty() ? "" : FACTORIES.getFirst().getNames().get(0);
     private Configuration _templateConfiguration;
@@ -106,92 +99,8 @@ public class ScriptResource extends JobRelatedResource {
     public Configuration getTemplateConfiguration(){
         return _templateConfiguration;
     }
-    
-    /**
-	 * JavaBean that packages script, its execution and the result.
-	 * 
-	 * TODO: the script could create an object that persists and retains a
-	 * reference to the Bindings. by encapsulating this way, I've made it
-	 * a bit more difficult? we could {@code eng} and {@code script} arguments
-	 * to execute() method.
-	 */
-    public static class ScriptExecution {
-    	private ScriptEngine eng;
-    	private ApplicationContext appCtx;
-    	private Bindings bindings;
-    	private String script;
-    	
-    	private StringWriter rawString;
-    	private StringWriter htmlString;
-    	private Throwable exception;
-    	private int linesExecuted;
-    	
-    	public ScriptExecution(ScriptEngine eng, ApplicationContext appCtx, String script) {
-    		this.eng = eng;
-    		this.appCtx = appCtx;
-    		this.bindings = new BeanLookupBindings(appCtx);
-    		this.script = script;
-    		if (StringUtils.isBlank(this.script)) {
-    			this.script = "";
-    		}
-    		this.rawString = new StringWriter();
-    		this.htmlString = new StringWriter();
-    	}
-    	public void bind(String name, Object obj) {
-    		bindings.put(name, obj);
-    	}
-    	public Object unbind(String name) {
-    		return bindings.remove(name);
-    	}
-    	public void execute() {
-    		PrintWriter rawOut = new PrintWriter(rawString);
-    		PrintWriter htmlOut = new PrintWriter(htmlString);
-    		bind("rawOut", rawOut);
-    		bind("htmlOut", htmlOut);
-    		bind("appCtx", appCtx);
-    		try {
-    			eng.eval(script, bindings);
-    			// TODO: should count with RE rather than creating String[]?
-    			linesExecuted = script.split("\r?\n").length;
-    		} catch (ScriptException ex) {
-    			Throwable cause = ex.getCause();
-    			exception = cause != null ? cause : ex;
-    		} catch (RuntimeException ex) {
-    			exception = ex;
-    		} finally {
-    			rawOut.flush();
-    			htmlOut.flush();
-    			// TODO: are these really necessary? 
-    			unbind("rawOut");
-    			unbind("htmlOut");
-    			unbind("appCtx");
-    		}
-    	}
-    	public boolean isFailure() {
-    		return exception != null;
-    	}
-    	public String getStackTrace() {
-    		if (exception == null) return "";
-    		StringWriter s = new StringWriter();
-    		exception.printStackTrace(new PrintWriter(s));
-    		return s.toString();
-    	}
-    	public Throwable getException() {
-    		return exception;
-    	}
-    	public int getLinesExecuted() {
-    		return linesExecuted;
-    	}
-    	public String getRawOutput() {
-    		return rawString.toString();
-    	}
-    	public String getHtmlOutput() {
-    		return htmlString.toString();
-    	}
-    	public String getScript() {
-            return script;
-        }
-    }
+        
+    private ScriptModel scriptModel;
     
     @Override
     public void acceptRepresentation(Representation entity) throws ResourceException {
@@ -204,12 +113,10 @@ public class ScriptResource extends JobRelatedResource {
 
         ScriptEngine eng = MANAGER.getEngineByName(chosenEngine);
         
-        scriptExec = new ScriptExecution(eng, cj.getJobContext(), script);
-        scriptExec.bind("job", cj);
-        scriptExec.bind("scriptResource", this);
-        scriptExec.execute();
-        scriptExec.unbind("job");
-        scriptExec.unbind("scriptResource");
+        scriptModel = makeDataModel();
+        scriptModel.bind("scriptResource", this);
+        scriptModel.execute(eng, script);
+        scriptModel.unbind("scriptResource");
         
         //TODO: log script, results somewhere; job log INFO? 
         
@@ -217,11 +124,14 @@ public class ScriptResource extends JobRelatedResource {
     }
     
     public Representation represent(Variant variant) throws ResourceException {
+        if (scriptModel == null) {
+            scriptModel = makeDataModel();
+        }
         Representation representation;
         if (variant.getMediaType() == MediaType.APPLICATION_XML) {
             representation = new WriterRepresentation(MediaType.APPLICATION_XML) {
                 public void write(Writer writer) throws IOException {
-                    XmlMarshaller.marshalDocument(writer,"script", makeDataModel());
+                    XmlMarshaller.marshalDocument(writer,"script", scriptModel);
                 }
             };
         } else {
@@ -262,10 +172,9 @@ public class ScriptResource extends JobRelatedResource {
         }
         Reference baseRefRef = new Reference(baseRef);
         
-        ScriptModel model = new ScriptModel(cj.getShortName(),
+        ScriptModel model = new ScriptModel(cj,
                 new Reference(baseRefRef, "..").getTargetRef().toString(),
-                getAvailableScriptEngines(),
-                scriptExec);
+                getAvailableScriptEngines());
 
         return model;
     }
@@ -284,7 +193,7 @@ public class ScriptResource extends JobRelatedResource {
         viewModel.put("cssRef", getStylesheetRef());
         viewModel.put("staticRef", getStaticRef(""));
         viewModel.put("baseResourceRef",getRequest().getRootRef().toString()+"/engine/static/");
-        viewModel.put("model",makeDataModel());
+        viewModel.put("model", scriptModel);
         viewModel.put("selectedEngine", chosenEngine);
 
         try {

engine/src/main/java/org/archive/crawler/restlet/XmlMarshaller.java
@@ -19,10 +19,23 @@
 
 package org.archive.crawler.restlet;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+
+import org.apache.commons.lang.StringUtils;
 import org.restlet.util.XmlWriter;
 import org.xml.sax.SAXException;
 
@@ -90,6 +103,88 @@ public class XmlMarshaller {
             }
         }
     }
+    /**
+     * sort PropertyDescriptors according to propOrder.
+     * properties listed in propOrder come first, in the order they are listed, and
+     * then come remaining unlisted properties, in arbitrary order (likely alphabetical).
+     * this semantics is rather relaxed compared to original JAXB semantics, where props
+     * and propOrder must be the same set (except for those marked XmlTransient).
+     * @param props PropertyDescriptor array to be sorted in-place.
+     * @param propOrder list of property names in order of desired appearance. 
+     */
+    protected static void orderProperties(PropertyDescriptor[] props, final String[] propOrder) {
+        if (propOrder == null || propOrder.length == 0) return;
+        final Map<String, Integer> order = new HashMap<String, Integer>();
+        for (int i = 0; i < propOrder.length; i++) {
+            order.put(propOrder[i], i);
+        }
+        final Integer LAST = Integer.valueOf(propOrder.length);
+        Arrays.sort(props, new Comparator<PropertyDescriptor>() {
+            @Override
+            public int compare(PropertyDescriptor o1, PropertyDescriptor o2) {
+                Integer c1 = order.get(o1.getName());
+                Integer c2 = order.get(o2.getName());
+                return (c1 != null ? c1 : LAST).compareTo(c2 != null ? c2 : LAST);
+            } 
+        });
+    }
+    /**
+     * test if {@code obj} has {@link XmlRootElement} annotation.
+     * to avoid unexpected side effects, objects are mapped to nested XML structure
+     * only when its class has {@link XmlRootElement} annotation.
+     * note semantics is slightly different from JAXB - just borrowing XmlRootElement
+     * as substitute of XmlElement, because Map entry cannot be annotated.
+     * @param obj object to test
+     * @return true if obj's class has XmlRootElement annotation.
+     */
+    protected static boolean marshalAsElement(Object obj) {
+    	XmlRootElement ann = obj.getClass().getAnnotation(XmlRootElement.class);
+    	return ann != null;
+    }
+    /**
+     * generate nested XML structure for a bean {@code obj}. enclosing element
+     * will not be generated if {@code key} is empty. each readable JavaBeans property
+     * is mapped to an nested element named after its name. Those properties
+     * annotated with {@link XmlTransient} are ignored.
+     * @param xmlWriter XmlWriter
+     * @param key name of enclosing element
+     * @param obj bean
+     * @throws SAXException
+     */
+    protected static void marshalBean(XmlWriter xmlWriter, String key, Object obj) throws SAXException {
+        if (!StringUtils.isEmpty(key))
+            xmlWriter.startElement(key);
+        try {
+            BeanInfo beanInfo = Introspector.getBeanInfo(obj.getClass(), Object.class);
+            PropertyDescriptor[] props = beanInfo.getPropertyDescriptors();
+            XmlType xmlType = obj.getClass().getAnnotation(XmlType.class);
+            if (xmlType != null) {
+                String[] propOrder = xmlType.propOrder();
+                if (propOrder != null) {
+                    // TODO: should cache this sorted version?
+                    orderProperties(props, propOrder);
+                }
+            }
+            for (PropertyDescriptor prop : props) {
+                Method m = prop.getReadMethod();
+                if (m == null || m.getAnnotation(XmlTransient.class) != null)
+                    continue;
+                try {
+                    Object propValue = m.invoke(obj);
+                    if (propValue != null && !"".equals(propValue)) {
+                        marshal(xmlWriter, prop.getName(), propValue);
+                    }
+                } catch (Exception ex) {
+                    // generate empty element, for now. generate comment?
+                    xmlWriter.emptyElement(prop.getName());
+                }
+            }
+        } catch (IntrospectionException ex) {
+            // ignored, for now.
+        }
+        if (!StringUtils.isEmpty(key))
+            xmlWriter.endElement(key);
+    }
 
     protected static void marshal(XmlWriter xmlWriter, String key, Object value) throws SAXException {
         if (value == null) {
@@ -98,6 +193,8 @@ public class XmlMarshaller {
             marshal(xmlWriter, key, (Map<?,?>) value);
         } else if (value instanceof Iterable<?>) {
             marshal(xmlWriter, key, (Iterable<?>) value);
+        } else if (marshalAsElement(value)) {
+            marshalBean(xmlWriter, key, value);
         } else {
             xmlWriter.dataElement(key, value.toString());
         }

engine/src/main/java/org/archive/crawler/restlet/models/ScriptModel.java
@@ -1,28 +1,51 @@
 package org.archive.crawler.restlet.models;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.archive.crawler.restlet.ScriptResource.ScriptExecution;
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
 
-@SuppressWarnings("serial")
-public class ScriptModel  extends LinkedHashMap<String, Object> {
+import org.archive.crawler.framework.BeanLookupBindings;
+import org.archive.crawler.framework.CrawlJob;
 
-	public ScriptModel(String crawlJobShortName, String crawlJobUrl,
-			Collection<Map<String, String>> scriptEngines,
-			ScriptExecution scriptExec) {// int linesExecuted, Exception
-											// exception, String rawOutput,
-											// String htmlOutput){
-        super();
-        this.put("crawlJobUrl",crawlJobUrl);
-        this.put("crawlJobShortName", crawlJobShortName);
-        this.put("availableScriptEngines", scriptEngines);
-        
-        if (scriptExec != null)
-        	this.put("scriptExec", scriptExec);
+@XmlRootElement(name="script")
+@XmlType(propOrder={
+        "crawlJobUrl", "crawlJobShortName", "availableScriptEngines", 
+        "script", "linesExecuted", "exception", "rawOutput", "htmlOutput",
+        "availableGlobalVariables"
+})
+public class ScriptModel {
+    private CrawlJob cj;
+    private String crawlJobUrl;
+    private Collection<Map<String, String>> availableScriptEngines;
+    
+    //private ApplicationContext appCtx;
+    private ScriptEngine eng;
+    private String script;
+    private Bindings bindings;
+    
+    private StringWriter rawString;
+    private StringWriter htmlString;
+    private Throwable exception;
+    private int linesExecuted;
+    
+    private List<Map<String, String>> availableGlobalVariables;
+
+	public ScriptModel(CrawlJob cj,
+	        String crawlJobUrl,
+			Collection<Map<String, String>> scriptEngines) {
+	    this.crawlJobUrl = crawlJobUrl;
+	    this.cj = cj;
+	    this.availableScriptEngines = scriptEngines;
         
         List<Map<String,String>> vars = new LinkedList<Map<String,String>>();
         Map<String,String> var;
@@ -51,6 +74,86 @@ public class ScriptModel  extends LinkedHashMap<String, Object> {
         var.put("variable", "scriptResource");
         var.put("description", "the ScriptResource implementing this page, which offers utility methods");
         vars.add(var);
-        this.put("availableGlobalVariables", vars);
+        
+        this.availableGlobalVariables = vars;
+        
+        this.bindings = new BeanLookupBindings(this.cj.getJobContext());
+        this.script = "";
+    }
+	
+    public void bind(String name, Object obj) {
+        bindings.put(name, obj);
+    }
+    public Object unbind(String name) {
+        return bindings.remove(name);
+    }
+    public void execute(ScriptEngine eng, String script) {
+        // TODO: update through setter rather than passing as method arguments?
+        this.eng = eng;
+        this.script = script;
+        
+        bind("job", cj);
+        rawString = new StringWriter();
+        htmlString = new StringWriter();
+        PrintWriter rawOut = new PrintWriter(rawString);
+        PrintWriter htmlOut = new PrintWriter(htmlString);
+        bind("rawOut", rawOut);
+        bind("htmlOut", htmlOut);
+        bind("appCtx", cj.getJobContext());
+        try {
+            this.eng.eval(this.script, bindings);
+            // TODO: should count with RE rather than creating String[]?
+            linesExecuted = script.split("\r?\n").length;
+        } catch (ScriptException ex) {
+            Throwable cause = ex.getCause();
+            exception = cause != null ? cause : ex;
+        } catch (RuntimeException ex) {
+            exception = ex;
+        } finally {
+            rawOut.flush();
+            htmlOut.flush();
+            // TODO: are these really necessary? 
+            unbind("rawOut");
+            unbind("htmlOut");
+            unbind("appCtx");
+            unbind("job");
+        }
+    }
+    public boolean isFailure() {
+        return exception != null;
+    }
+    public String getStackTrace() {
+        if (exception == null) return "";
+        StringWriter s = new StringWriter();
+        exception.printStackTrace(new PrintWriter(s));
+        return s.toString();
+    }
+    public Throwable getException() {
+        return exception;
+    }
+    public int getLinesExecuted() {
+        return linesExecuted;
+    }
+    public String getRawOutput() {
+        return rawString != null ? rawString.toString() : "";
+    }
+    public String getHtmlOutput() {
+        return htmlString != null ? htmlString.toString() : "";
+    }
+    public String getScript() {
+        return script;
+    }
+    
+    public String getCrawlJobShortName() {
+        return cj.getShortName();
+    }
+    public Collection<Map<String, String>> getAvailableScriptEngines() {
+        return availableScriptEngines;
+    }
+    public List<Map<String, String>> getAvailableGlobalVariables() {
+        return availableGlobalVariables;
+    }
+    public String getCrawlJobUrl() {
+        return crawlJobUrl;
     }
 }

engine/src/main/resources/org/archive/crawler/restlet/Script.ftl
@@ -15,28 +15,25 @@
 </head>
 <body>
 	<h1>Execute script for job <i><a href='/engine/job/${model.crawlJobShortName}'>${model.crawlJobShortName}</a></i></h1>
-
-    <#if model.scriptExec??>
-	<#if (model.scriptExec.linesExecuted > 0)>
-	<span class='success'>${model.scriptExec.linesExecuted} ${(model.scriptExec.linesExecuted>1)?string("lines","line")} executed<span>
+	<#if (model.linesExecuted > 0)>
+	<span class='success'>${model.linesExecuted} ${(model.linesExecuted>1)?string("lines","line")} executed<span>
 	</#if>
-	<#if model.scriptExec.failure>
-	<pre style='color:red; height:150px; overflow:auto'>${model.scriptExec.stackTrace}
+	<#if model.failure>
+	<pre style='color:red; height:150px; overflow:auto'>${model.stackTrace}
 	</pre>
 	</#if>
-	<#assign htmlOutput=model.scriptExec.htmlOutput>
+	<#assign htmlOutput=model.htmlOutput>
 	<#if (htmlOutput?length > 0)>
 	<fieldset><legend>htmlOut</legend>
 	${htmlOutput}
 	</fieldset>
 	</#if>
-	<#assign rawOutput=model.scriptExec.rawOutput>
+	<#assign rawOutput=model.rawOutput>
 	<#if (rawOutput?length > 0)>
 	<fieldset><legend>rawOutput</legend>
 		<pre style="margin:0;">${rawOutput}
 		</pre>
 	</fieldset>
-	</#if>
 	</#if>
 
 	<form method="POST">
@@ -46,7 +43,7 @@
 			<option<#if selectedEngine=scriptEngine.engine> selected="selected"</#if> value="${scriptEngine.engine}">${scriptEngine.language}</option>
 			</#list>
 		</select>
-		<textarea rows='20' style='width:100%' name='script' id='editor'>${(model.scriptExec.script)!""}</textarea>
+		<textarea rows='20' style='width:100%' name='script' id='editor'>${(model.script)!""}</textarea>
 		<input type='submit' value='execute'></input>
 	</form>
 	The script will be executed in an engine preloaded
@@ -69,7 +66,3 @@
 	</script>
 </body>
 </html>
-
-
-
-