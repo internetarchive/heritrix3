df2a313797b15d01e3959e309842b25ebb92ca91
Noah Levitt <nlevitt@archive.org>
2013-11-15 17:20:55 -0800

    throw exception when host cache returns 0.0.0.0 for any host (trying to track down a strange problem)

--- modules/src/main/java/org/archive/modules/fetcher/DefaultServerCache.java
+++ modules/src/main/java/org/archive/modules/fetcher/DefaultServerCache.java
@@ -107,6 +107,9 @@ public class DefaultServerCache extends ServerCache implements Closeable, Serial
                         String hkey = new String(hostname); // ensure private minimal key
                         return new CrawlHost(hkey);
                     }});
+        if ("0.0.0.0".equals(host.getIP().getHostAddress())) {
+            throw new IllegalStateException("got suspicious value 0.0.0.0 for " + hostname);
+        }
         return host;
     }
 
