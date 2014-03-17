fb039291effb9627380dc583a05e79066087353d
Noah Levitt <nlevitt@archive.org>
2014-03-16 18:14:29 -0700

    fix indentation (spaces only)

--- contrib/src/main/java/org/archive/modules/deciderules/ExpressionDecideRule.java
+++ contrib/src/main/java/org/archive/modules/deciderules/ExpressionDecideRule.java
@@ -37,9 +37,9 @@ import org.archive.modules.CrawlURI;
  * @contributor nlevitt
  */
 public class ExpressionDecideRule extends PredicatedDecideRule {
-	private static final long serialVersionUID = 1L;
-	
-	private static final Logger logger =
+    private static final long serialVersionUID = 1L;
+
+    private static final Logger logger =
             Logger.getLogger(ExpressionDecideRule.class.getName());
 
     {
@@ -52,26 +52,26 @@ public class ExpressionDecideRule extends PredicatedDecideRule {
         return (String) kp.get("groovyExpression");
     }
 
-	protected ConcurrentHashMap<String,Template> groovyTemplates = new ConcurrentHashMap<String, Template>();
-	protected Template groovyTemplate() {
-		Template groovyTemplate = groovyTemplates.get(getGroovyExpression());
+    protected ConcurrentHashMap<String,Template> groovyTemplates = new ConcurrentHashMap<String, Template>();
+    protected Template groovyTemplate() {
+        Template groovyTemplate = groovyTemplates.get(getGroovyExpression());
 
-		if (groovyTemplate == null) {
-			try {
-				groovyTemplate = new SimpleTemplateEngine().createTemplate(getGroovyExpression());
-				groovyTemplates.put(getGroovyExpression(), groovyTemplate);
-			} catch (Exception e) {
-				logger.log(Level.SEVERE, "problem with groovy expression " + getGroovyExpression(), e);
-			}
-		}
+        if (groovyTemplate == null) {
+            try {
+                groovyTemplate = new SimpleTemplateEngine().createTemplate(getGroovyExpression());
+                groovyTemplates.put(getGroovyExpression(), groovyTemplate);
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, "problem with groovy expression " + getGroovyExpression(), e);
+            }
+        }
 
-		return groovyTemplate;
-	}
-	
-	@Override
-	protected boolean evaluate(CrawlURI curi) {
+        return groovyTemplate;
+    }
+
+    @Override
+    protected boolean evaluate(CrawlURI curi) {
         HashMap<String, Object> binding = new HashMap<String, Object>();
         binding.put("curi", curi);
-		return String.valueOf(true).equals(groovyTemplate().make(binding).toString());
-	}
+        return String.valueOf(true).equals(groovyTemplate().make(binding).toString());
+    }
 }
