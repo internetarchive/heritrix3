c0f57a5f549a269ed4c64e15d7285f5a0898aaec
Barbara Miller <galgeek@me.com>
2020-05-29 17:56:56 -0700

    use ffmpeg for segmented downloads

--- contrib/src/main/java/org/archive/modules/extractor/ExtractorYoutubeDL.java
+++ contrib/src/main/java/org/archive/modules/extractor/ExtractorYoutubeDL.java
@@ -420,6 +420,7 @@ public class ExtractorYoutubeDL extends Extractor
          */
         ProcessBuilder pb = new ProcessBuilder("youtube-dl", "--ignore-config",
                 "--simulate", "--dump-single-json", "--format=best[height <=? 576]",
+                "--hls-prefer-ffmpeg",
                 "--playlist-end=" + MAX_VIDEOS_PER_PAGE, uri.toString());
         logger.info("running: " + String.join(" ", pb.command()));
 
