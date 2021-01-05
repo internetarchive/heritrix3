0649751456f66e512bab01782604ef421d0ce586
Barbara Miller <galgeek@me.com>
2021-01-04 17:53:46 -0800

    --no-cache-dir, --no-playlist

--- contrib/src/main/java/org/archive/modules/extractor/ExtractorYoutubeDL.java
+++ contrib/src/main/java/org/archive/modules/extractor/ExtractorYoutubeDL.java
@@ -420,6 +420,7 @@ public class ExtractorYoutubeDL extends Extractor
          */
         ProcessBuilder pb = new ProcessBuilder("youtube-dl", "--ignore-config",
                 "--simulate", "--dump-single-json", "--format=best[height <=? 576]",
+                "--no-cache-dir", "--no-playlist",
                 "--playlist-end=" + MAX_VIDEOS_PER_PAGE, uri.toString());
         logger.info("running: " + String.join(" ", pb.command()));
 
