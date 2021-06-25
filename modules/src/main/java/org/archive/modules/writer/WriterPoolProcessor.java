6b42c608a56cee375defd93b8c8392e5192357f8
Alex Osborne <aosborne@nla.gov.au>
2021-06-25 14:15:39 +0900

    BaseWARCRecordBuilder: remove now unused getHostAddress(curi) helper
    We now save the server IP at fetch time and retrieve it with
    CrawlURI.getServerIP()

--- modules/src/main/java/org/archive/modules/writer/WriterPoolProcessor.java
+++ modules/src/main/java/org/archive/modules/writer/WriterPoolProcessor.java
@@ -372,7 +372,7 @@ implements Lifecycle, Checkpointable, WriterPoolSettings {
      * @param curi CrawlURI
      * @return String of IP address
      * 
-     * @deprecated WARCRecordBuilder instances use {@link BaseWARCRecordBuilder#getHostAddress(CrawlURI)}
+     * @deprecated WARCRecordBuilder instances use {@link CrawlURI#getServerIP()}
      */
     @Deprecated
     protected String getHostAddress(CrawlURI curi) {
