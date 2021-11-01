internetarchive/heritrix3

commit 045b2516da91768af42ddd6736b4b8ff9455f691

Update to latest version of dnsjava, for #344
This commit updates to version 3.3.1 of the dnsjava library.

Incidentally, the restlet.org certificate is currently invalid, so this
patch switched to maven.restlet.com.

modules/src/main/java/org/archive/modules/fetcher/FetchDNS.java
@@ -239,7 +239,7 @@ public class FetchDNS extends Processor {
         try {
         	recordDNS(curi, rrecordSet);
             curi.setFetchStatus(S_DNS_SUCCESS);
-            curi.setDNSServerIPLabel(ResolverConfig.getCurrentConfig().server());
+            curi.setDNSServerIPLabel(ResolverConfig.getCurrentConfig().server().toString());
         } catch (IOException e) {
         	logger.log(Level.SEVERE, "Failed store of DNS Record for " +
         		curi.toString(), e);

commons/pom.xml
@@ -85,9 +85,9 @@
 			<scope>compile</scope>
 		</dependency>
 		<dependency>
-			<groupId>org.dnsjava</groupId>
+			<groupId>dnsjava</groupId>
 			<artifactId>dnsjava</artifactId>
-			<version>2.0.3</version>
+			<version>3.3.1</version>
 			<scope>compile</scope>
 		</dependency>
 		<!--

engine/pom.xml
@@ -14,7 +14,7 @@
 	<repositories>
 		<repository>
 			<id>maven-restlet</id>
-			<url>https://maven.restlet.org</url>
+			<url>https://maven.restlet.com</url>
 		</repository>
 	</repositories>
 

commit 1832c5ef89534a930c86e9aa5af5b4cbd55921c8

FetchDNS: Keep dnsjava selector thread out of ToePool
dnsjava will create its global selector thread on first DNS lookup.
Since the first lookup occurs on a toe thread then this will cause the
selector thead to join the job's toe thread group. This is a problem
because when the job is terminated ToePool sends an interrupt to all
threads in the thread group which the dnsjava selector thread is
not expected and never clears. The interrupted state then causes
select() to return immediately instead of sleeping so it consumes
100% CPU.

So workaround this by doing a dummy lookup when FetchDNS is loaded,
this ensures the first lookup happens on the web server thread that
handles the job build action instead of a toe thread.

Fixes: 045b2516da91 ("Update to latest version of dnsjava")

modules/src/main/java/org/archive/modules/fetcher/FetchDNS.java
@@ -69,6 +69,14 @@ public class FetchDNS extends Processor {
     static {
         // cap size at 1 (we never want a cached value; 0 is non-operative)
         Lookup.getDefaultCache(DClass.IN).setMaxEntries(1);
+
+        // do a dummy lookup to force the creation of dnsjava NIO selector thread
+        // ensures it doesn't end up in the toe-threads group
+        try {
+            new Lookup("localhost").run();
+        } catch (TextParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     // Defaults.