3396970d27fcf8ef4ae848617b7e0a7334f524bf
Barbara Miller <barbara@archive.org>
2024-03-18 15:38:25 -0700

    vcodec,res

--- contrib/src/main/java/org/archive/modules/extractor/ExtractorYoutubeDL.java
+++ contrib/src/main/java/org/archive/modules/extractor/ExtractorYoutubeDL.java
@@ -442,8 +442,8 @@ public class ExtractorYoutubeDL extends Extractor
          * with video and audio.
          * https://github.com/ytdl-org/youtube-dl/blob/master/README.md#format-selection
          */
-        ProcessBuilder pb = new ProcessBuilder("youtube-dl", "--ignore-config",
-                "--simulate", "--dump-single-json", "--format=best[height <=? 576]",
+        ProcessBuilder pb = new ProcessBuilder("nice", "-n", Integer.toString(NICE_MOD), "yt-dlp", "--ignore-config",
+                "--simulate", "--dump-single-json", "-S vcodec:h264,res:576,acodec:aac",
                 "--no-cache-dir", "--no-playlist",
                 "--playlist-end=" + MAX_VIDEOS_PER_PAGE, uri.toString());
         logger.info("running: " + String.join(" ", pb.command()));
